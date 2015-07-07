c1fdaaa9958aa99b01809192b2d1160bb943f17a
brecht <brecht.baeten@gmail.com>
2015-07-07 14:10:31 +0200

    reordered classes

--- mpcpy.py
+++ mpcpy.py
@@ -8,6 +8,47 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+###########################################################################
+class Boundaryconditions:
+	def __init__(self,bcs,periodic=True):
+		"""
+		Arguments:
+		bcs:     		a dict with the actual boundary conditions and a time vector
+		"""
+		
+		self.boundaryconditions = {}
+		
+		if periodic:
+			# the entire dataset is repeated 3 times, once before the actual data, the actual data and once after the actual data
+			# this implies the control horizon must be shorter than the boundary conditions dataset
+			self.boundaryconditions['time'] = np.concatenate((bcs['time'][:-1]-bcs['time'][-1],bcs['time'],bcs['time'][1:]+bcs['time'][-1]))
+			for key in bcs:
+				if key != 'time':
+					self.boundaryconditions[key] =  np.concatenate((bcs[key][:-1],bcs[key],bcs[key][1:]))
+		else:
+			# 1st and last value are repeated before and after the actual data
+			self.boundaryconditions['time'] = np.concatenate((bcs['time'][:-1]-bcs['time'][-1],bcs['time'],bcs['time'][1:]+bcs['time'][-1]))
+			for key in bcs:
+				if key != 'time':
+					self.boundaryconditions[key] =  np.concatenate((bcs[key][0]*np.ones(len(bcs['time'][:-1])),bcs[key],bcs[key][-1]*np.ones(len(bcs['time'][1:]))))
+			
+		
+	def __call__(self,time):
+		"""
+		Return the interpolated boundary conditions
+		
+		Arguments:
+		time:   true value for time
+		"""
+		
+		bcs_int = {}
+		for key in self.boundaryconditions:
+			bcs_int[key] = np.interp(time,self.boundaryconditions['time'],self.boundaryconditions[key])
+		
+		return bcs_int
+		
+
 ###########################################################################
 class Emulator:
 	def __init__(self,dymola,inputs,initializationtime = 1):
@@ -87,47 +128,6 @@ class Emulator:
 			for key in res.keys():
 				self.res[key] = np.interp(input['time'],res['time'],res[key])
 			
-
-
-###########################################################################
-class Boundaryconditions:
-	def __init__(self,bcs,periodic=True):
-		"""
-		Arguments:
-		bcs:     		a dict with the actual boundary conditions and a time vector
-		"""
-		
-		self.boundaryconditions = {}
-		
-		if periodic:
-			# the entire dataset is repeated 3 times, once before the actual data, the actual data and once after the actual data
-			# this implies the control horizon must be shorter than the boundary conditions dataset
-			self.boundaryconditions['time'] = np.concatenate((bcs['time'][:-1]-bcs['time'][-1],bcs['time'],bcs['time'][1:]+bcs['time'][-1]))
-			for key in bcs:
-				if key != 'time':
-					self.boundaryconditions[key] =  np.concatenate((bcs[key][:-1],bcs[key],bcs[key][1:]))
-		else:
-			# 1st and last value are repeated before and after the actual data
-			self.boundaryconditions['time'] = np.concatenate((bcs['time'][:-1]-bcs['time'][-1],bcs['time'],bcs['time'][1:]+bcs['time'][-1]))
-			for key in bcs:
-				if key != 'time':
-					self.boundaryconditions[key] =  np.concatenate((bcs[key][0]*np.ones(len(bcs['time'][:-1])),bcs[key],bcs[key][-1]*np.ones(len(bcs['time'][1:]))))
-			
-		
-	def __call__(self,time):
-		"""
-		Return the interpolated boundary conditions
-		
-		Arguments:
-		time:   true value for time
-		"""
-		
-		bcs_int = {}
-		for key in self.boundaryconditions:
-			bcs_int[key] = np.interp(time,self.boundaryconditions['time'],self.boundaryconditions[key])
-		
-		return bcs_int
-		
 		
 ###########################################################################
 class Stateestimation:
