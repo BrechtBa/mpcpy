61c5f0ac5b9cab71501a26b5fff993203258e777
brecht <brecht.baeten@gmail.com>
2015-10-02 15:02:01 +0200

    added setup script and changed folder structure

--- mpcpy/mpcpy.py
+++ mpcpy/mpcpy.py
@@ -1,13 +1,25 @@
-#!/usr/bin/env python
-# mpcPy
-# Documentation
+#!/usr/bin/python
+######################################################################################
+#    Copyright 2015 Brecht Baeten
+#    This file is part of mpcpy.
+#
+#    mpcpy is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    mpcpy is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with mpcpy.  If not, see <http://www.gnu.org/licenses/>.
+######################################################################################
 
 
-# import libraries
 import sys
 import numpy as np
-import matplotlib.pyplot as plt
-
 
 ###########################################################################
 class Boundaryconditions:
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='mpcpy',
+    version='0.0.1',
+    license='GNU GENERAL PUBLIC LICENSE',
+	description='A package to run mpc simulations in Python with Dymola or other simulation packages',
+	url='https://github.com/BrechtBa/mpcpy',
+	author='Brecht Baeten',
+	author_email='brecht.baeten@gmail.com',
+	packages=['mpcpy'],
+	install_requires=['numpy'],
+	classifiers = ['Programming Language :: Python :: 2.7'],
+)
