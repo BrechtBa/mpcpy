f41a12669712ca25acb6cf8dc9d05c5b8b3ff22d
brecht <brecht.baeten@gmail.com>
2016-05-10 15:04:47 +0200

    updated version number in setup py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python
 from setuptools import setup
 
 setup(
     name='mpcpy',
-    version='0.0.2',
+    version='0.1.0',
     license='GNU GENERAL PUBLIC LICENSE',
 	description='A package to run mpc simulations in Python with Dymola or other simulation packages',
 	url='https://github.com/BrechtBa/mpcpy',
