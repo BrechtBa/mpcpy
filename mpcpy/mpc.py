9148cfbede997a53430c2de6a3fd2f0cfc98a47d
Brecht Baeten <brecht.baeten@enervalis.com>
2017-10-28 19:55:03 +0200

    added future print function for python 2.7 compatibility

--- mpcpy/mpc.py
+++ mpcpy/mpc.py
@@ -17,7 +17,7 @@
 #    along with mpcpy.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-import sys
+from __future__ import print_function
 import numpy as np
 
 
